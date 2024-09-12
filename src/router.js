773f1451518a27623bd7f6939b072ba2589756da
Eunike Pardede <eunikehedriani@gmail.com>
2024-09-12 11:46:00 +0200

    Adding Route Page Components

--- src/router.js
+++ src/router.js
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import MentorDetail from './pages/mentors/MentorDetail.vue';
+import MentorRegistration from './pages/mentors/MentorRegistration.vue';
+import MentorsList from './pages/mentors/MentorsList.vue';
+import ContactMentor from './pages/requests/ContactMentor.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/mentors' },
-        { path: '/mentors', component: null },
-        { path: '/mentors/:id', component: null, children: [
-            { path: 'contact', component: null}
+        { path: '/mentors', component: MentorsList },
+        { path: '/mentors/:id', component: MentorDetail, children: [
+            { path: 'contact', component: ContactMentor}
         ]},
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound(.*)', component: null},
+        { path: '/register', component: MentorRegistration },
+        { path: '/requests', component: RequestsReceived },
+        { path: '/:notFound(.*)', component: NotFound},
     ],
 })
 
