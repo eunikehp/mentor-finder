d288167574708636abc9cfd97881ed2e0ecb25c2
Eunike Pardede <eunikehedriani@gmail.com>
2024-09-13 17:09:17 +0200

    Registering as a Mentor: The Form - Create MentorForm - Add data with empty space and null - Bind the input with data by using v-model , add modifier .number on rate and .trim on desc (trim excess white space) - add submitForm method, to be triggered after form is submitted. create formData for data input - add submit event with prevent modifier to prevent the default of an automatic request being sent to server that doesn't exist. then bind the submitForm. - add styling - import MentorForm component on the MentorRegistration page

--- src/pages/mentors/MentorRegistration.vue
+++ src/pages/mentors/MentorRegistration.vue
@@ -1,3 +1,16 @@
 <template>
-    <section>register</section>
+  <section>
+    <base-card>
+      <h2>Register as a mentor now!</h2>
+      <mentor-form></mentor-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import MentorForm from '@/components/mentors/MentorForm.vue';
+
+export default {
+    components: {MentorForm}
+};
+</script>MentorForm
