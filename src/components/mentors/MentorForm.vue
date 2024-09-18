1ffac95a2cc6c8989a78dd0119787f26c84661ef
Eunike Pardede <eunikehedriani@gmail.com>
2024-09-18 18:40:41 +0200

    Adding form validation - Add validateForm methods in the MentorForm - put in validateForm in the submitForm, once the data is submitted we also validate it - Add data 'formIsValid' and add value and isValid on data - Update v-model bindings by adding .val - add if conditional inside submitForm , if form is invalid, data will not be saved - show user some feedback if inputs are invalid - binding a class on the template, with css class invalid when input is invalid - add new paragraph below each input and trigger it when input is empty with v-if - fix formData by adding .val behind each data.
    To clear the red after we enter the valid value
    - add new method 'clearValidity and pass input as an argument
    - call the clearValidity function with onblur event. The onblur event occurs when an HTML element loses focus. often used with form validation (when the user leaves a form field).

--- src/components/mentors/MentorForm.vue
+++ src/components/mentors/MentorForm.vue
@@ -1,36 +1,42 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !firstName.isValid}"> <!-- add css class invalid if input is invalid-->
       <label for="firstname">Firstname</label>
-      <input type="text" id="firstname" v-model.trim="firstName" />
+      <input type="text" id="firstname" v-model.trim="firstName.val" @blur="clearValidity('firstName')"/>
+      <p v-if="!firstName.isValid">First name must be not empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !lastName.isValid}">
       <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" v-model.trim="lastName" />
+      <input type="text" id="lastname" v-model.trim="lastName.val" @blur="clearValidity('lastName')"/>
+      <p v-if="!lastName.isValid">Last name must be not empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !description.isValid}">
       <label for="description">Description</label>
-      <input type="text" id="description" v-model.trim="description" />
+      <input type="text" id="description" v-model.trim="description.val" @blur="clearValidity('description')"/>
+      <p v-if="!description.isValid">Description must be not empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !rate.isValid}">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" v-model.number="rate" />
+      <input type="number" id="rate" v-model.number="rate.val" @blur="clearValidity('rate')"/>
+      <p v-if="!rate.isValid">Rate must be greater than 0.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: !areas.isValid}">
       <h3>Areas of Expertise</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas.val" @blur="clearValidity('areas')"/>
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <input type="checkbox" id="backend" value="backend" v-model="areas.val" @blur="clearValidity('areas')"/>
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career" v-model="areas" />
+        <input type="checkbox" id="career" value="career" v-model="areas.val" @blur="clearValidity('areas')"/>
         <label for="career">Career Advisory</label>
       </div>
+      <p v-if="!areas.isValid">At least one expertise must be selected</p>
     </div>
+    <p v-if="!formIsValid">Please fix the errors above and submit again</p>
     <base-button>Register</base-button>
   </form>
 </template>
@@ -40,25 +46,72 @@ export default {
   emits: ["save-data"],
   data() {
     return {
-      firstName: "",
-      lastName: "",
-      description: "",
-      rate: null,
-      areas: [],
+      firstName: {
+        val: '',
+        isValid: true
+      },
+      lastName: {
+        val: '',
+        isValid: true
+      },
+      description: {
+        val: '',
+        isValid: true
+      },
+      rate: {
+        val: null,
+        isValid: true
+      },
+      areas: {
+        val: [],
+        isValid: true
+      },
+      formIsValid: true
     };
   },
   methods: {
     submitForm() {
+      this.validateForm();
+      if (!this.formIsValid) {
+        return; // if form is invalid/ false then this return /stop doesnt go to formData
+      }
       const formData = {
-        first: this.firstName,
-        last: this.lastName,
-        rate: this.rate,
-        desc: this.description,
-        areas: this.areas,
+        first: this.firstName.val,
+        last: this.lastName.val,
+        rate: this.rate.val,
+        desc: this.description.val,
+        areas: this.areas.val,
       };
       console.log(formData);
       this.$emit("save-data", formData);
     },
+    validateForm() {
+      this.formIsValid = true;
+      if (this.firstName.val === '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.lastName.val === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.description.val === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      }
+      if (!this.rate.val || this.rate.val < 0) {
+        this.rate.isValid = false;
+        this.formIsValid = false;
+      }
+      if (this.areas.val.length === 0) {
+        this.areas.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+    // to clear the red after we enter the valid value
+    clearValidity(input) {
+      this[input].isValid = true;
+    }
   },
 };
 </script>
