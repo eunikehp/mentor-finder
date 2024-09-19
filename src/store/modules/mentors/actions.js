30199098f24babb863b55e5753865b090b13c6b8
Eunike Pardede <eunikehedriani@gmail.com>
2024-09-19 19:11:12 +0200

    Sending a PUT Http Request to Store Mentor Data - set up Firebase project and create realtime database (website) - add fetch url in actions and method PUT to send the data to the server - store ID in a separate constant userId - edit the url by adding slash userId - add body to convert the object into JSON - add promise using async await syntax - get responseData by awaiting response.json
    // data is stored in the backend (firebase) but it doesn't show up on the app after reloading

--- src/store/modules/mentors/actions.js
+++ src/store/modules/mentors/actions.js
@@ -1,8 +1,10 @@
 //because keyname of database doesnt align with keynames of emitted data, so use actions to transform the data
 export default {
-    addMentor(context, data){
+    async addMentor(context, data){
+        //store ID in a separate constant userId
+        const userId = context.rootGetters.userId;
         const mentorData = {
-            id: context.rootGetters.userId,
+            // id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
@@ -10,6 +12,28 @@ export default {
             areas: data.areas
         };
 
-        context.commit('addMentor', mentorData);
+        // send the request to server, tells the firebase, data will be overwritten if it existed or created if didn't exist
+        const response = await fetch(`https://react-mentor-finder-default-rtdb.firebaseio.com/mentors/${userId}.json`, {
+            method: 'PUT' ,
+            body: JSON.stringify(mentorData) //convert object into JSON format //sends a put request to this URL with this data
+        });
+
+        // const responseData = await response.json();
+        
+        //check if the response is ok
+        if(!response.ok) {
+            //error
+        }
+
+        //commit data by copying mentorData with spread operator (take all key value pairs in mentorData)
+        // and merge them into a new object. add ID field, which holds the userId
+        context.commit('addMentor', { 
+            ...mentorData,
+            id: userId
+    });
     }
 };
+
+
+// POST = a new entry would be added all the time
+// PUT = data will be overwritten if it exists , if not it would be created
