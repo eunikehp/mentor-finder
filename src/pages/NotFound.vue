1c83b0c7ea7cbb5e77199df0803bd1b15919063d
Eunike Pardede <eunikehedriani@gmail.com>
2024-10-08 18:23:56 +0200

    The "Not Found" Page - add template for not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,16 @@
 <template>
-    <section></section>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>This page could not be found - maybe check out our <router-link to="/mentors"> mentors</router-link>.</p>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import BaseCard from '@/components/ui/BaseCard.vue';
+
+export default {
+    components: {BaseCard}
+}
+</script>
