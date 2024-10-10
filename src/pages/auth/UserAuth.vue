103d1e591499660ab4f7e77eb4da9fb6169a46b7
Eunike Pardede <eunikehedriani@gmail.com>
2024-10-10 13:25:57 +0200

    Loading Spinner and Error Handling for SignUp action - add 'isLoading and error' data property in UserAuth - update the method 'submitForm', and add promise (async & await) - wrap with try catch, if there is error , error message will appear - add 2 base-dialog for error and isLoading

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,74 +1,105 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">Email</label>
-        <input type="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password"/>
-      </div>
-        <p v-if="!formIsValid">Please enter a valid email and password (must be at least 6 characters long).</p>
+  <div>
+    <base-dialog :show='!!error' title='An error occured' @close="handleError"> <!-- !! is to convert it into Boolean-->
+        <p>{{ error }}</p>
+    </base-dialog> 
+    <base-dialog :show='isLoading' title='Authenticating...' fixed></base-dialog> <!-- fixed = without close button-->
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">Email</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password (must be at least 6 characters
+          long).
+        </p>
         <base-button>{{ submitButtonCaption }}</base-button>
-        <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeBtnCaption}}</base-button>
-    </form>
-  </base-card>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeBtnCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
+import BaseDialog from "@/components/ui/BaseDialog.vue";
 export default {
-    data(){
-        return {
-            email: "",
-            password: "",
-            formIsValid: true,
-            mode: "login"
-        }
+  components: { BaseDialog },
+  data() {
+    return {
+      email: "",
+      password: "",
+      formIsValid: true,
+      mode: "login",
+      isLoading: false,
+      error: null,
+    };
+  },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === "login") {
+        return "Login";
+      } else {
+        return "Signup";
+      }
     },
-    computed: {
-        submitButtonCaption(){
-            if(this.mode === 'login') {
-                return 'Login';
-            } else {
-                return 'Signup';
-            }
-        },
-        switchModeBtnCaption () {
-            if(this.mode === 'login') {
-                return 'Signup instead';
-            } else {
-                return 'Login Instead';
-            }
-        }
+    switchModeBtnCaption() {
+      if (this.mode === "login") {
+        return "Signup instead";
+      } else {
+        return "Login Instead";
+      }
+    },
+  },
+  methods: {
+    async submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === "" ||
+        !this.email.includes("@") ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+        return;
+      }
 
-    },
-    methods: {
-        submitForm(){
-            this.formIsValid =true;
-            if(this.email === '' || !this.email.includes('@') || this.password.length < 6) {
-                this.formIsValid=false;
-                return;
-            }
-            //send HTTP request
-            if(this.mode === 'login'){
-                //...
-            } else {
-                this.$store.dispatch('signup', {
-                    email: this.email,
-                    password: this.password,
-                })
-            }
-        },
-        switchAuthMode(){
-            if(this.mode === 'login') {
-                this.mode = 'signup'
-            } else {
-                this.mode = 'login'
-            }
+      this.isLoading = true;
+
+      try {
+        //send HTTP request
+        if (this.mode === "login") {
+          //...
+        } else {
+          await this.$store.dispatch("signup", {
+            email: this.email,
+            password: this.password,
+          });
         }
+      } catch (err) {
+        this.error = err.message || "Failed to authenticate, try later.";
+      }
+
+      this.isLoading = false;
+    },
+    switchAuthMode() {
+      if (this.mode === "login") {
+        this.mode = "signup";
+      } else {
+        this.mode = "login";
+      }
+    },
+    handleError() {
+        this.error = null;
     }
-}</script>
+  },
+};
+</script>
 
 <style scoped>
 form {
