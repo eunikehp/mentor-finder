5e22a2205b22cf479dbd94800d580ad5c5d32dd8
Eunike Pardede <eunikehedriani@gmail.com>
2024-10-11 17:02:56 +0200

    Adding "Auto-login" to Ensure the user can stay log in - on the Auth actions, add new action 'auth' , copy login action. - refactor the login and signup action, so make it code simple.
    Store the token , userId, and tokenExpiration not just in Vuex, but also some storage that survives page reload. Local storage = API built into the browser storage, which can be accessed from inside JS
    - in auth actions,  access localStorage and store data in local storage
    - add new action 'tryLogin' to get data from local storage then commit setUser.  we check whether data exists. if yes, we set a user, otherwise not. so auto login
    
    Dispatch 'tryLogin' action in App.vue, so whenever app starts user can log in automatically
    - in App.vue, add created lifecycle hook, then dispatch 'tryLogin' action.

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,59 +1,75 @@
 export default {
-  async login(context,payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBqKX44iMM6ztnb4jXWwMJoS7_bf8z_0X8',{
-        method: 'POST',
-        body: JSON.stringify({
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true
-        })
-        
-    });
-    const responseData = await response.json();
-
-    if(!response.ok) {
-        const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.') 
-        throw error;
-    }
-
-    console.log(responseData);
-    context.commit('setUser', {
-        token: responseData.idToken,
-        userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn
+  async login(context, payload) {
+    return context.dispatch('auth', {
+        ...payload,
+        mode: 'login'
     })
   },
   async signup(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBqKX44iMM6ztnb4jXWwMJoS7_bf8z_0X8',{
-        method: 'POST',
-        body: JSON.stringify({
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true
-        })
-        
+    return context.dispatch('auth', {
+        ...payload,
+        mode: 'signup'
+    })
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBqKX44iMM6ztnb4jXWwMJoS7_bf8z_0X8";
+
+    if (mode === "signup") {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBqKX44iMM6ztnb4jXWwMJoS7_bf8z_0X8";
+    }
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
     });
     const responseData = await response.json();
 
-    if(!response.ok) {
-        const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.') 
-        throw error;
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || "Failed to authenticate. Check your login data."
+      );
+      throw error;
     }
 
-    console.log(responseData);
-    context.commit('setUser', {
-        token: responseData.idToken,
-        userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn
-    })
+    //access localStorage and store data in localstorage
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
+
+    context.commit("setUser", {
+      token: responseData.idToken, //from firebase auth
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
   },
-  logout(context){
-    context.commit('setUser',{
-        token: null,
-        userId: null,
-        tokenExpiration: null
-    })
-  }
+  //autologin 
+  //check whether data exists. if yes, we set a user, otherwise not.
+  tryLogin(context) {
+    //access the local storage and get the item
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
 
+    //check if there are token and userID, then commit set user and pass data on 
+    if (token && userId) {
+        context.commit('setUser', {
+            token: token,
+            userId: userId,
+            tokenExpiration: null
+        })
+    }
+  },
+  logout(context) {
+    context.commit("setUser", {
+      token: null,
+      userId: null,
+      tokenExpiration: null,
+    });
+  },
 };
-
